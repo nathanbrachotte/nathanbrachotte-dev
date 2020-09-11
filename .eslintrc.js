3d3f60466246fa22e387a17728ed5a82b5a3c5cf
Nathan Brachotte <nathan.brachotte@klarna.com>
2020-09-11 09:08:30 +0200

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,13 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  // extends: 'eslint:recommended',
-  extends: 'react-app',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+  extends: ['airbnb', 'prettier', 'plugin:cypress/recommended'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/require-default-props': 'off',
   },
   parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
   },
-  plugins: ['react'],
-  rules: {
-    // 'no-use-before-define': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/prop-types': 'off',
-    'comma-dangle': 'off',
-    'no-unused-vars': 'warn',
-    'no-console': 'warn',
-    'prettier/prettier': 'off',
-  },
-}
+};
