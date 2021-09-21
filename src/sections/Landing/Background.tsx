fb02dbd8310e30e34cef9954ac8923486e0c3893
Nathan Brachotte <nathan.brachotte@klarna.com>
2021-09-21 16:17:23 +0200

    lang(🇪🇺): 🏳 add spanish

--- src/sections/Landing/Background.tsx
+++ src/sections/Landing/Background.tsx
@@ -3,36 +3,19 @@ import Triangle from '../../shared/Triangle'
 
 const Background: React.FC = () => (
   <div>
-    {/* bottom left */}
-    {/* <Triangle
-      color="backgroundDark"
-      height={['38vh', '80vh']}
-      width={['50vw', '35vw']}
-      invertY
-      invertX
-    /> */}
     {/* top left */}
-    {/* <Triangle
-      color="backgroundDark"
-      height={['20vh', '15vh']}
-      width={['98vw', '90vw']}
-    /> */}
-
+    <Triangle
+      color="primaryDark"
+      height={['10vh', '0vh']}
+      width={['100vw', '100vw']}
+    />
     {/* top right */}
     <Triangle
       color="primaryDark"
-      height={['20vh', '20vh']}
-      width={['80vw', '80vw']}
+      height={['12vh', '15vh']}
+      width={['100vw', '80vw']}
       invertX
     />
-
-    {/* left */}
-    {/* <Triangle
-      color="secondary"
-      height={['38vh', '80vh']}
-      width={['50vw', '35vw']}
-    /> */}
-
     {/* bottom left */}
     <Triangle
       color="primaryLight"
