eeb793348813a5a4103675d3af80b8adafbab010
Nathan Brachotte <nathan.brachotte+github@gmail.com>
2022-04-15 14:27:20 +0200

    refactor(Helmet): 💡add extra tags in helmet

--- src/Helmet.tsx
+++ src/Helmet.tsx
@@ -43,29 +43,38 @@ const Helmet: React.FC<Props> = ({ theme = {} }) => (
 
       return (
         <ReactHelmet htmlAttributes={{ lang: 'en' }}>
-          <meta charSet="utf-8" />
           <title>{title}</title>
+          <meta charSet="utf-8" />
+          <meta name="title" content={title} />
           <meta name="description" content={description} />
+
           <link rel="shortcut icon" href={logoUrl} />
           <meta name="theme-color" content={theme.background} />
           <meta name="image" content={logoUrl} />
           <meta itemProp="name" content={title} />
           <meta itemProp="description" content={description} />
           <meta itemProp="image" content={logoUrl} />
+          <link rel="apple-touch-icon" sizes="180x180" href={logoUrl} />
+          <link rel="icon" type="image/png" sizes="32x32" href={logoUrl} />
+          <link rel="icon" type="image/png" sizes="16x16" href={logoUrl} />
+
+          {/* START: Twitter */}
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:title" content={title} />
+          <meta property="twitter:url" content="https://nathanbrachotte.dev/" />
+          <meta name="twitter:description" content={description} />
+          <meta name="twitter:image" content={logoUrl} />
+          <meta name="twitter:image:src" content={logoUrl} />
+          {/* END: Twitter */}
+          {/* START: Open graph / Facebook */}
+          <meta property="og:url" content="https://nathanbrachotte.dev" />
           <meta name="og:title" content={title} />
           <meta name="og:description" content={description} />
           <meta name="og:image" content={logoUrl} />
           <meta name="og:site_name" content={title} />
           <meta name="og:locale" content="en_US" />
           <meta name="og:type" content="website" />
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:title" content={title} />
-          <meta name="twitter:description" content={description} />
-          <meta name="twitter:image" content={logoUrl} />
-          <meta name="twitter:image:src" content={logoUrl} />
-          <link rel="apple-touch-icon" sizes="180x180" href={logoUrl} />
-          <link rel="icon" type="image/png" sizes="32x32" href={logoUrl} />
-          <link rel="icon" type="image/png" sizes="16x16" href={logoUrl} />
+          {/* END: Open graph / Facebook */}
         </ReactHelmet>
       )
     }}
